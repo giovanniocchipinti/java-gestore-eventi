d80b65e5073db9f2dd2eb8b9b7d52f84e8e1f56c
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2025-01-10 18:44:39 +0100

    feat: added control to validate that total places number is positive.

--- src/com_java_events/Event.java
+++ src/com_java_events/Event.java
@@ -3,14 +3,29 @@ package com_java_events;
 public class Event {
     private String titolo;
     private int data;
-    private int numPostiTotale = 0;
-    private int numPostiRiservati;
+    private int numPostiTotale;
+    private int numPostiRiservati = 0;
 
-    public Event(String titolo, int data, int numPostiRiservati){
+    public Event(String titolo, int data, int numPostiTotale){
         this.titolo = titolo;
         this.data = data;
-        this.numPostiRiservati = numPostiRiservati;
+        this.numPostiTotale = numPostiTotale;
     }
 
+    public void posti (){
+        if (numPostiTotale >= 0) {    
+        }
+        else {
+            System.out.println("inserire un numero valido");
+        }
+    }
+    
 
 }
+
+
+    
+
+
+
+
