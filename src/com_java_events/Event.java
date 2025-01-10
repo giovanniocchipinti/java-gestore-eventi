6e558f44c6ac249c9afe0f155f531dc8ede51a70
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2025-01-10 19:00:45 +0100

    feat: added getter and setter methods;

--- src/com_java_events/Event.java
+++ src/com_java_events/Event.java
@@ -1,17 +1,21 @@
 package com_java_events;
 
 public class Event {
+
+    // variabili d'istanza
     private String titolo;
     private int data;
     private int numPostiTotale;
     private int numPostiRiservati = 0;
 
+    // costruttore
     public Event(String titolo, int data, int numPostiTotale){
         this.titolo = titolo;
         this.data = data;
         this.numPostiTotale = numPostiTotale;
     }
 
+    // controllo posti totali positivi
     public void posti (){
         if (numPostiTotale >= 0) {    
         }
@@ -20,7 +24,31 @@ public class Event {
         }
     }
     
+    // metodi getter
+    public String getTitle(){
+        return titolo;
+    }
 
+    public int getDate(){
+        return data;
+    }
+
+    public int getTotalPlaces(){
+        return numPostiTotale;
+    }
+
+    public int getReservedPlaces(){
+        return numPostiRiservati;
+    }
+
+    // metodi setter
+    public void setTitle(String titolo){
+        this.titolo = titolo;
+    }
+
+    public void setDate(int data){
+        this.data = data;
+    }
 }
 
 
