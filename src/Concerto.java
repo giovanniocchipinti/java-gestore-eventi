3be9dce504f8e2cb51210b89e372e01192d153dd
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2025-08-24 12:09:15 +0200

    feat: added getter and setter methods to "Concerto" class

--- src/Concerto.java
+++ src/Concerto.java
@@ -2,14 +2,31 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Concerto extends Evento{
-
+    // variabili d'istanza
     private LocalTime ora;
     private double prezzo;
 
+    // costruttore
     Concerto(String titolo, LocalDate data, int postiTotali, LocalTime ora, double prezzo) {
         super(titolo, data, postiTotali);
         this.ora = ora;
         this.prezzo = prezzo;
     }
 
+    // metodi getter & setter
+    public LocalTime getOra() {
+        return ora;
+    }
+    public void setOra(LocalTime ora) {
+        this.ora = ora;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    
 }
