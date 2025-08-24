ee2ae50cfadd37cdcd6924f77df739dbeba3ced1
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2025-08-24 09:56:56 +0200

    feat: created new class

---
+++ src/Evento.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class Evento {
+    private String titolo;
+    private LocalDate data;
+    private final int postiTotali;
+    private int postiPrenotati;
+
+    Evento(String titolo, LocalDate data, int postiTotali){
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data non può essere già passata!");
+        }
+        if (postiTotali <= 0) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo!");
+        }
+
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
+}
